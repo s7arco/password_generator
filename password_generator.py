eca46b855eed917fe7c347092675b11454a0b664
jb02982 <jb02982@surrey.ac.uk>
2026-01-25 19:03:56 +0000

    cleaned up code got rid of unecessary variable

--- password_generator.py
+++ password_generator.py
@@ -2,8 +2,7 @@ import random
 import string
 
 def password_generator(length):
-    password = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
-    return password
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
 
 print("Password Generator")
 user_length = int(input("Password Length: "))
