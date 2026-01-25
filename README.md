a7b34fee4293dd9bf788b627e93d10b0b1898c71
jb02982 <jb02982@surrey.ac.uk>
2026-01-25 21:15:52 +0000

    Added copy button and updated readme

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ A simple password generator made in Python.
 Generates secure 5 digit passwords
 
 ## Current Features 
-- Generates random 5 digit password
+- Generates random 5-12 digit password
+- GUI screen
+- Allows password to be copied into clipboard
 
 ## Plans
-- Allow user to define length
-- Add symbols
-- Make a GUI 
+- Store into database SQLite
 
 ## How to Run 
 ```bash python password_generator.py
--- password_generator.py
+++ password_generator.py
@@ -9,15 +9,21 @@ def password_generator():
          result_label.config(text="Enter a valid number")
          return
     
-    if length > 12:
-         result_label.config(text="Error Max Length: 12")
+    if length > 18:
+         result_label.config(text="Error Max Length: ")
          return
     
     if length < 5:
-         result_label.config(text="Error Minimum Length: 5")
+         result_label.config(text="Error Minimum Length: 6")
+         return
 
     password = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
     result_label.config(text=password)
+
+def copy_password():
+     password = result_label.cget("text")
+     window.clipboard_clear
+     window.clipboard_append(password)
 
 # Setup window
 window = tk.Tk()
@@ -27,17 +33,22 @@ window.geometry("500x300")
 # Title & PLength input
 Title = tk.Label(text="Password Generator",
                  background="White").pack(pady=20)
-label = tk.Label(text="Password Length:")
+label = tk.Label(text="Password Length: ")
 plength_entry = tk.Entry()
 label.pack()
 plength_entry.pack()
 
-# Create button
+# Create button to generate password
 button = tk.Button(text="Generate", command=password_generator)
 button.pack(pady=20)
 
+# Button to copy onto clipboard for pasting
+copy_button = tk.Button(text="Copy", command=copy_password)
+
+
 result_label = tk.Label(text="")
-result_label.pack()
+result_label.pack(pady=10)
+copy_button.pack(pady=20)
 
 
 window.mainloop()
